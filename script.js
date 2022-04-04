c7a0cd5f4b45fa7eee24dd62f6066365d1a5da49
oxica <kskaplun@gmail.com>
2022-04-04 23:40:52 +0300

    fix scroll

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.querySelectorAll(".story-btn").forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.classList.toggle("change");
@@ -6,4 +8,28 @@ document.querySelectorAll(".story-btn").forEach((btn) => {
 });
 
 
+ function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
 
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back-to-top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back-to-top-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, window.innerHeight);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.back-to-top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+
+  
